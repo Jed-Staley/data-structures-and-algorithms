170532f53a465aded93f37e451435bbf007f9ab2
Jed Staley <JNStaley1@gmail.com>
2024-03-28 08:24:33 -0400

    Did challenge 7

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -107,7 +107,8 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  let newArr = arr.filter((object) => object.baseStat > minBaseStat);
+  return newArr.map((object) => object.stat.name);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -254,7 +255,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
