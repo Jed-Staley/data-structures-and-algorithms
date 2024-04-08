486e8a59bcb278428fffd40193e1746bdff68720
Jed Staley <JNStaley1@gmail.com>
2024-04-08 15:30:57 -0400

    Did challenge 2

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -16,9 +16,7 @@ Write a function named firstLetters that takes in an array of strings and return
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 'w', 'w', ':']
 ------------------------------------------------------------------------------------------------ */
 
-const firstLetters = (arr) => {
-  // Solution code here...
-};
+const firstLetters = arr => arr.map((str) => str[0]);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
