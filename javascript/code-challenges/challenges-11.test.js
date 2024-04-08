bbc3708d79aebd41f8f26b1b4a1b08549245f4af
Jed Staley <JNStaley1@gmail.com>
2024-04-08 15:33:17 -0400

    Did challenge 3

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -26,9 +26,7 @@ Write a function named findHappiness that takes in an array of strings and retur
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this is great :)', ':)))))']
 ------------------------------------------------------------------------------------------------ */
 
-const findHappiness = (arr) => {
-  // Solution code here...
-};
+const findHappiness = arr => arr.filter((str) => /:\)/.test(str));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
