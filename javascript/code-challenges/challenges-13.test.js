edf877a7a20a5794f860d34b7dcb52a84c069102
Jed Staley <JNStaley1@gmail.com>
2024-04-10 16:17:36 -0400

    Did challenge 2

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -61,9 +61,7 @@ Write a function named containsW that takes in a string. This function should us
 
 ------------------------------------------------------------------------------------------------ */
 
-const containsW = (str) => {
-  // Solution code here...
-};
+const containsW = str => /w/.test(str);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
