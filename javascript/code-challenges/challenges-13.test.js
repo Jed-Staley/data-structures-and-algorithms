ea1b1ba743b1046f96c6179767d5b185c25d4330
Jed Staley <JNStaley1@gmail.com>
2024-04-10 16:18:25 -0400

    Did challenge 4

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -84,9 +84,7 @@ Write a function named containsWorld that takes in a string or number of any len
 
 ------------------------------------------------------------------------------------------------ */
 
-const containsWorld = (input) => {
-  // Solution code here...
-};
+const containsWorld = input => /world/.test(input);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
