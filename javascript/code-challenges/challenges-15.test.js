4e6c3b68b671ed5f10c1a275352daa1f60e906ce
Jed Staley <JNStaley1@gmail.com>
2024-04-12 22:19:34 -0400

    Did challenge 6

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -145,7 +145,22 @@ Here is a sample board:
 ------------------------------------------------------------------------------------------------ */
 
 const detectTicTacToeWin = (board) => {
-  // Solution code here...
+  for (let i = 0; i < 3; i++) {
+    if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+      return true;
+    }
+  }
+
+  for (let j = 0; j < 3; j++) {
+    if (board[0][j] !== '' && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+      return true;
+    }
+  }
+
+  if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {return true;}
+  if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {return true;}
+
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
